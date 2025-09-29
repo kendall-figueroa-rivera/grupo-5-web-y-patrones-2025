b7b51afafbf07d6a05ecff78192ebfe3b95dccae
kendall-figueroa-rivera <kendallfigueroa21@gmail.com>
2025-09-29 17:42:38 -0600

    first commit
    creacion de ambiente

---
+++ src/test/java/TiendaTech/TiendaTechApplicationTests.java
@@ -0,0 +1,13 @@
+package TiendaTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
