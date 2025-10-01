fc5d3004e7c2391e35baa78f40ba3c76610497ac
kendall-figueroa-rivera <kendallfigueroa21@gmail.com>
2025-10-01 00:16:51 -0600

    second commit
    cambios entre clase 2 y 3

---
+++ src/main/java/TiendaTech/service/CategoriaServices.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package TiendaTech.service;
+import TiendaTech.domain.Categoria;
+
+import TiendaTech.repository.CategoriaRepository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Service;
+
+import org.springframework.transaction.annotation.Transactional;
+ 
+@Service
+
+public class CategoriaServices {
+ 
+    @Autowired
+
+    private CategoriaRepository categoriaRepository;
+
+    @Transactional(readOnly=true)
+
+    public List<Categoria> getCategorias(boolean activo) {
+
+        if (activo) {
+
+            return categoriaRepository.findByActivoTrue();
+
+        }
+
+        return categoriaRepository.findAll();
+
+    }
+
+
+}
+ 
